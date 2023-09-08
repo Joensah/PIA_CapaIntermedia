8a37b46f58c07faa49656ff38da259fa6caa74bd
JoEn <116038291+Joensah@users.noreply.github.com>
2023-09-08 00:37:09 -0600

    first commit

---
+++ Index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PIA</title>
+
+        <!--del icono pestaña navegador-->
+    <link rel="shortcut icon" href="img/rayo.png" type="image/x-icon">
+
+        <!--hoja de estilos de la pagina principal-->
+    <link rel="stylesheet" href="css/Home.css">
+
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+
+<header>
+        <!--Barra de navegación-->
+        <nav>
+            
+            <a href="Index.php">Inicio</a>
+            <a href="Consulta_Categorias.php">Categorias</a>
+            <a href="Consulta_Compras.php">Compras</a>
+            <a href="Consulta_Ventas.php">Ventas</a>
+            <a href="Ver_Carrito.php">Carrito</a>
+            <a href="Ver_Pedidos.php">Pedidos</a>
+            <a href="#Section6">Cotizaciones</a>
+           
+        </nav>
+
+        <!--Logo-->
+        <img  class="Logo" src="img/logo1.png" width="200" height="75" alt="Imagen">
+
+        <!--Banner promocional-->
+        <section class="textos-header">
+            <h1>Descuentos de hasta el 75%*</h1>
+            <h4>*Aplican restricciones</h4>
+        </section>
+    </header>
+
+
+    <!--Secciones-->
+    <main>
+    <section class="Novedades">
+            <h2 class="titulo">Novedades</h2>
+            
+        </section>
+    </main>
+
+
+        <!--Pie de página-->
+    <footer>
+        <h3 class="copyright">Copyright ©TiendaOnline 2023. All Right Reserved</h3>
+    </footer>
+
+        <!-- JavaScript-->
+        <script src></script>
+    
+        
+</body>
+
+</html>
